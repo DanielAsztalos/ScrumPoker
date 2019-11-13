a83bdbe7916c903269cc030251a03f3d1a565100
danielasztalos <danielasztalosb@gmail.com>
2019-11-13 17:32:59 +0200

    Modified User model

--- app/src/main/java/com/example/scrumpoker/model/User.java
+++ app/src/main/java/com/example/scrumpoker/model/User.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class User {
     private int id;
     private String username;
+    private String email;
+    private String password;
     private Role role;
     private ArrayList<Integer> groupIds;
 
@@ -50,5 +52,21 @@ public class User {
 
     public void removeGroupId(int id) {
         this.groupIds.remove(id);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
